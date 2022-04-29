a2180ee4c9fe6082a59a2333618a1a7a711a849f
bhumiharsaurabh <100083442+bhumiharsaurabh@users.noreply.github.com>
2022-04-29 10:52:11 +0530

    KATIL MUSICX

--- handlers/song.py
+++ handlers/song.py
@@ -59,7 +59,7 @@ def song(client, message):
         message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit("**𝐘𝐨𝐮𝐭𝐮𝐛𝐞  𝐄𝐫𝐫𝐨𝐫 ❌ 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐎𝐰𝐧𝐞𝐫 🥀 [★•亗『𝐊𝐀𝐓𝐈𝐋』亗•★](https://t.me/tera_baap_katil) ❤️**")
+        m.edit("**𝐘𝐨𝐮𝐭𝐮𝐛𝐞  𝐄𝐫𝐫𝐨𝐫 ❌ 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐎𝐰𝐧𝐞𝐫 🥀 [★•亗『𝐊𝐀𝐓𝐈𝐋』亗•★](https://t.me/tera_baap_katil)❤️ to Fix it !!**")
         print(e)
 
     try:
