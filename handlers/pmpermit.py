2df64b678c165ecaa99503216cf79eacefa7d1d1
bhumiharsaurabh <100083442+bhumiharsaurabh@users.noreply.github.com>
2022-04-29 10:39:53 +0530

    KATIL MUSICX

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -32,11 +32,11 @@ async def bye(client: Client, message: Message):
         queryy = text[1]
         if queryy == "on":
             PMSET = True
-            await message.reply_text("PM Permit Enabled")
+            await message.reply_text("PM Permit Enabled ✅")
             return
         if queryy == "off":
             PMSET = None
-            await message.reply_text("PM Permit Disabled")
+            await message.reply_text("PM Permit Disabled ❌")
             return
 
 @USER.on_message(filters.text & filters.private & filters.me)        
@@ -44,7 +44,7 @@ async def autopmPermiat(client: USER, message: Message):
     chat_id = message.chat.id
     if not chat_id in pchats:
         pchats.append(chat_id)
-        await message.reply_text("Auto Approved ...")
+        await message.reply_text("Auto Approved ❗...")
         return
     message.continue_propagation()    
     
@@ -53,7 +53,7 @@ async def pmPermiat(client: USER, message: Message):
     chat_id = message.chat.id
     if not chat_id in pchats:
         pchats.append(chat_id)
-        await message.reply_text("Approoved to PM")
+        await message.reply_text("Approoved to PM ✅")
         return
     message.continue_propagation()    
     
@@ -63,6 +63,6 @@ async def rmpmPermiat(client: USER, message: Message):
     chat_id = message.chat.id
     if chat_id in pchats:
         pchats.remove(chat_id)
-        await message.reply_text("Dispprooved to PM")
+        await message.reply_text("Dispprooved to PM ❌")
         return
     message.continue_propagation()
