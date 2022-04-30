98287da39dfea4d09bc4842137f33903deea0a0d
BHUMIHAR SAURABH (KATIL) <100083442+bhumiharsaurabh@users.noreply.github.com>
2022-04-30 07:24:26 +0530

    KATIL MUSICX

--- handlers/extra.py
+++ handlers/extra.py
@@ -12,7 +12,7 @@ from config import (
 
 @Client.on_message(command(["start", f"start@{BOT_USERNAME}"]) & filters.group & ~filters.edited)
 async def help(client: Client, message: Message):
-    await message.reply_text("**𝐓𝐇𝐀𝐍𝐊𝐒 𝐅𝐎𝐑 𝐀𝐃𝐃𝐈𝐍𝐆 𝐌𝐄 𝐈𝐍 𝐘𝐎𝐔𝐑 𝐆𝐑𝐎𝐔𝐏 ❤️ 𝐍𝐎𝐖 𝐏𝐑𝐎𝐌𝐎𝐓𝐄 𝐌𝐄 𝐀𝐒 𝐀𝐃𝐌𝐈𝐍𝐈𝐒𝐓𝐑𝐀𝐓𝐎𝐑 𝐈𝐍 𝐓𝐇𝐈𝐒 𝐂𝐇𝐀𝐓 𝐖𝐈𝐓𝐇 𝐍𝐄𝐄𝐃𝐄𝐃 𝐏𝐎𝐖𝐄𝐑 𝐎𝐓𝐇𝐄𝐑𝐖𝐈𝐒𝐄 𝐈 𝐀𝐌 𝐍𝐎𝐓 In this Chat with needed powers otherwise I am not able to work properly !!**",
+    await message.reply_text("**𝐓𝐇𝐀𝐍𝐊𝐒 𝐅𝐎𝐑 𝐀𝐃𝐃𝐈𝐍𝐆 𝐌𝐄 𝐈𝐍 𝐘𝐎𝐔𝐑 𝐆𝐑𝐎𝐔𝐏 ❤️ 𝐍𝐎𝐖 𝐏𝐑𝐎𝐌𝐎𝐓𝐄 𝐌𝐄 𝐀𝐒 𝐀𝐃𝐌𝐈𝐍𝐈𝐒𝐓𝐑𝐀𝐓𝐎𝐑 𝐈𝐍 𝐓𝐇𝐈𝐒 𝐂𝐇𝐀𝐓 𝐖𝐈𝐓𝐇 𝐍𝐄𝐄𝐃𝐄𝐃 𝐏𝐎𝐖𝐄𝐑 𝐎𝐓𝐇𝐄𝐑𝐖𝐈𝐒𝐄 𝐈 𝐀𝐌 𝐍𝐎𝐓 𝐀𝐁𝐋𝐄 𝐓𝐎 𝐖𝐎𝐑𝐊 𝐏𝐑𝐎𝐏𝐄𝐑𝐋𝐘 !!**",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
