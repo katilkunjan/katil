56ac3229b81c3475f4e74ef820de93f198f91a5c
BHUMIHAR SAURABH (KATIL) <100083442+bhumiharsaurabh@users.noreply.github.com>
2022-05-02 21:41:59 +0530

    Update broadcast.py

--- handlers/broadcast.py
+++ handlers/broadcast.py
@@ -9,7 +9,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Dialog, Chat, Message
 from pyrogram.errors import UserAlreadyParticipant
 
-from callsmusic.callsmusic import client as aditya
+from callsmusic.callsmusic import client as katil
 from config import SUDO_USERS
 
 @Client.on_message(filters.command(["gcast"]))
